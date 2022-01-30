9e4c9c1a0427c9faeecacf903ffb8cc6bc5274aa
Byungjin Park <posquit0.bj@gmail.com>
2022-01-31 00:03:59 +0900

    Init repositories workspace

---
+++ repositories/terraform.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "tedilabs"
+
+    workspaces {
+      name = "github-cloud-tedilabs-repositories"
+    }
+  }
+}
+
+
+###################################################
+# Local Variables
+###################################################
+
+locals {
+  context = yamldecode(file(var.config_file)).context
+  config  = yamldecode(templatefile(var.config_file, local.context))
+}
+
+
+###################################################
+# Providers
+###################################################
+
+provider "github" {
+  owner = local.context.organization
+}
