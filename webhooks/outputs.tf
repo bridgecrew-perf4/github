936bbd9791864f908b0d47f33dbd0bb381b52366
Byungjin Park <posquit0.bj@gmail.com>
2022-02-03 02:06:26 +0900

    Add webhooks workspace

---
+++ webhooks/outputs.tf
@@ -0,0 +1,3 @@
+output "webhooks" {
+  value = module.webhook
+}
---
+++ webhooks/webhooks.tf
@@ -0,0 +1,18 @@
+module "webhook" {
+  source = "../modules/webhook"
+
+  for_each = {
+    for webhook in local.config.webhooks :
+    webhook.url => webhook
+  }
+
+  url          = each.key
+  content_type = try(each.value.content_type, "JSON")
+  ssl_enabled  = try(each.value.ssl_enabled, true)
+  secret       = try(each.value.secret, "")
+
+  repositories = try(each.value.repositories, ["*"])
+  events       = try(each.value.events, ["push"])
+
+  enabled = try(each.value.enabled, true)
+}
